1b66c3a98ea3503edd06909a660b6ebf29ff7bfe
ronakruparel <ronak.ruparel@accionlabs.com>
2019-04-09 11:20:50 +0530

    feat(login):added input field and button for login

---
+++ src/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+export default function LoginPage() {
+    return (
+        <div className="login-form">
+            <input type="text" name="username" />
+            <input type="text" />
+            <button>Submit</button>
+        </div>
+    );
+}
